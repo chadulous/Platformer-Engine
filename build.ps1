2184139a61fdb2a78e86b06014d1764f70e72017
MaverickQuill55 (VS Code) <VScode@toxik.cf>
2021-09-20 12:08:36 -0400

    auto commit

--- build.ps1
+++ build.ps1
@@ -1,7 +1,7 @@
 $ver = $args[0]
 python -m PyInstaller --noconfirm --onedir --windowed --name "Maria" --add-data "maps;maps/" --add-data "BAHNSCHRIFT.TTF;."  "main.py"
 Compress-Archive "dist/Maria/*" "dist.zip" -Force
-git add .
+git add -A
 git commit -am "auto commit"
 git push
 gh release create $ver 'dist.zip#Windows (Windows only for now)' --title $ver -n " "
